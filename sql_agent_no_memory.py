4d46896418a7485a8a9e3d7aae90c113acdc0156
SepKeyPro <sepehr.keykhaie@gmail.com>
2024-06-08 18:52:29 -0400

    Initial commit

---
+++ sql_agent_no_memory.py
@@ -0,0 +1,49 @@
+import psycopg2
+from langchain.llms import OpenAI
+from langchain.sql_database import SQLDatabase
+from langchain_experimental.sql import SQLDatabaseChain
+from langchain.memory import ConversationBufferMemory
+import os
+from getpass import getpass
+
+os.environ['OPENAI_API_KEY'] = getpass('Enter your API Key: ')
+
+
+# Setup database
+db = SQLDatabase.from_uri(
+    f"postgresql+psycopg2://postgres:Mahsa3504@localhost:5432/Office",
+)
+
+API_KEY = os.getenv('OPENAI_API_KEY')
+llm = OpenAI(temperature=0, openai_api_key=API_KEY)
+
+# Create db chain
+QUERY = """
+You are a home agent. Given an input question, first create a syntactically correct postgresql query to run, then look at the results of the query and return the answer. If a user asks about "now" it means current date and time.\n\n
+
+{question}
+
+Answer:
+"""
+
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+
+# Setup the database chain
+db_chain = SQLDatabaseChain(llm=llm, database=db, verbose=True, memory=memory)
+
+def chat():
+    print("Chatbot: Hi! How can I help you today?")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ['exit', 'quit', 'bye']:
+            print("Chatbot: Goodbye!")
+            break
+        try:
+            question = QUERY.format(question=user_input)
+            response = db_chain.run(question)
+            print(f"Chatbot: {response}")
+        except Exception as e:
+            print(e)
+    
+chat()
